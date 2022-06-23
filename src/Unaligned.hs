37463e0f7e57075113492bdd155792b8463aaf96
David Lahm <david@klinger-lahm.de>
2022-06-23 23:16:03 +0200

    removed Orientation kind

--- src/Unaligned.hs
+++ src/Unaligned.hs
@@ -22,44 +22,22 @@ import GHC.Exts
 
 type Bitcount = Word
 
-data Orientation = LeftOpen | RightOpen
 
--- | To use Orientation in contexts
 
-type family IsOrientation (t :: Orientation) :: Constraint where
-    IsOrientation LeftOpen = ()
-    IsOrientation RightOpen = ()
-
-data Unaligned (p :: Orientation) integral = Unaligned integral Int
+data LeftOpen integral = LeftOpen integral Int
   deriving (Eq)
 
--- | Obtain the type of integral used from an Unaligned object.
-type family EmbeddedWord a where
-  EmbeddedWord (Unaligned 'LeftOpen i) = i
-  EmbeddedWord (Unaligned 'RightOpen i) = i
-
--- | This class requires the implementation of a method to obtain the number of bits that are in use in the embedded integral and a smart constructor that ascertain that all unused bits are set to zero.
-class (Integral (EmbeddedWord a), FiniteBits (EmbeddedWord a)) => UnalignedContainer a where
-  usedBits :: a -> Int
-  makeUnaligned ::
-    EmbeddedWord a ->
-    Int ->
-    a
-
--- | Gives the number corresponding to a mask of size wordSize with a number of unsetBits unset at the left end of the mask.
-makeMask setBits = 2 ^ setBits - 1
-
-instance (Integral i, FiniteBits i) => UnalignedContainer (Unaligned 'LeftOpen i) where
-  usedBits (Unaligned _ n) = n
-  makeUnaligned integral n = Unaligned unusedToZero n
+makeLeftOpen integral n = LeftOpen unusedToZero n
     where
       unusedToZero =
         let mask = fromIntegral $ makeMask n
          in fromIntegral $ mask .&. integral
 
-instance (Integral i, FiniteBits i) => UnalignedContainer (Unaligned 'RightOpen i) where
-  usedBits (Unaligned _ n) = n
-  makeUnaligned integral n = Unaligned unusedToZero n
+
+data RightOpen integral = RightOpen integral Int
+  deriving (Eq)
+
+makeRightOpen integral n = RightOpen unusedToZero n
     where
       unusedToZero =
         let mask =
@@ -68,51 +46,50 @@ instance (Integral i, FiniteBits i) => UnalignedContainer (Unaligned 'RightOpen 
                 (finiteBitSize integral - n)
          in fromIntegral $ mask .&. integral
 
-instance (Show i) => Show (Unaligned p i) where
-  show (Unaligned x _) = show x
+-- | Gives the number corresponding to a mask of size wordSize with a number of unsetBits unset at the left end of the mask.
+makeMask setBits = 2 ^ setBits - 1
+
+instance (Show i) => Show (LeftOpen i) where
+    show (LeftOpen x _) = show x
+
+instance (Show i) => Show (RightOpen i) where
+    show (RightOpen x _) = show x
 
 -- | Non-empty Bytestring with designated incomplete first or last byte. This way, there is no need to deal with emptiness here, as the byte to be operated upon can be obtained by simple pattern matching.
-data UnalignedBytestring (p :: Orientation) where
-  (:>) :: ByteString -> Unaligned RightOpen Word8 -> UnalignedBytestring RightOpen
-  (:<) :: Unaligned LeftOpen Word8 -> ByteString -> UnalignedBytestring LeftOpen
+data RightOpenByteString = 
+     ByteString :> RightOpen Word8
 
-deriving instance Show (UnalignedBytestring p)
-deriving instance Eq (UnalignedBytestring p)
+data LeftOpenByteString =
+     LeftOpen Word8 :< ByteString
 
-type family ByteStringOrientation a where
-    ByteStringOrientation (UnalignedBytestring o) = o
--- | Provide a common interface for LeftOpen and RightOpen ByteStrings
-class UnalignedByteStringInterface i where
--- | Smart Constructor for UnalignedBytestring 
-    makeUnalignedByteString :: 
-           ByteString 
-        -> Int 
-        -> Maybe (UnalignedBytestring (ByteStringOrientation i)) 
--- | UnalignedBytestring to ByteString
-    toString :: UnalignedBytestring (ByteStringOrientation i) -> ByteString
+deriving instance Show RightOpenByteString
+deriving instance Eq RightOpenByteString
+
+deriving instance Show LeftOpenByteString
+deriving instance Eq LeftOpenByteString
 
 
-instance UnalignedByteStringInterface (UnalignedBytestring 'RightOpen)  where
-    makeUnalignedByteString bs used =
-     if BS.null bs 
-        then Nothing 
-        else    Just 
-              $ BS.take (BS.length bs - 1) bs :> makeUnaligned (BS.last bs) used
-    toString (Unaligned byte n :< bs) = BS.singleton byte `append` bs
+makeRightOpenByteString bs used =
+     if BS.null bs
+        then Nothing
+        else    Just
+              $ BS.take (BS.length bs - 1) bs :> makeRightOpen (BS.last bs) used
 
 
-instance UnalignedByteStringInterface (UnalignedBytestring 'LeftOpen)  where
-    makeUnalignedByteString bs used =
-     if BS.null bs 
-        then Nothing 
-        else    Just 
-              $ makeUnaligned (BS.head bs) used :< BS.tail bs 
-    toString (bs :> Unaligned byte n) = bs `append` BS.singleton byte
+makeLeftOpenByteString bs used =
+     if BS.null bs
+        then Nothing
+        else    Just
+              $ makeLeftOpen (BS.head bs) used :< BS.tail bs
 
+class ToByteString a where
+    toByteString :: a -> ByteString
 
--- | Get the last (unaligned) byte of an UnalignedBytestring RightOpen
-lastByte :: UnalignedBytestring RightOpen -> Unaligned RightOpen Word8
-lastByte (_ :> w) = w
+instance ToByteString RightOpenByteString where
+    toByteString (bs :> RightOpen byte n) = bs `BS.append` singleton byte
+
+instance ToByteString LeftOpenByteString where
+    toByteString (LeftOpen byte n :< bs) = singleton byte `BS.append` bs 
 
 -- | Get the left byte of a 16 Bit word
 leftByte :: Word16 -> Word8
@@ -125,65 +102,65 @@ rightByte word = fromIntegral $ word .&. 255
 -- | Combine to bytes into a Word16
 combineTwoBytes :: Word8 -> Word8 -> Word16
 combineTwoBytes leftByte rightByte =
-  let leftByte16 = fromIntegral @_ @Word16 leftByte
-      rightByte16 = fromIntegral @_ @Word16 rightByte
-   in shiftL leftByte16 8 `xor` rightByte16
+ let leftByte16 = fromIntegral @_ @Word16 leftByte
+     rightByte16 = fromIntegral @_ @Word16 rightByte
+    in shiftL leftByte16 8 `xor` rightByte16
 
 -- | Push a right-packed unaligned 16-Bit word into an unaligned left-packed Bytestring.
 pushWord ::
-  UnalignedBytestring RightOpen ->
-  Unaligned LeftOpen Word16 ->
-  UnalignedBytestring RightOpen
-pushWord (bs :> (Unaligned lastByte usedLeft)) (Unaligned word usedRight) =
-  let unusedLeft = 8 - usedLeft
-      unusedRight = 16 - usedRight
-      shiftValue = unusedRight - usedLeft
-      wordAdjusted =
-        if shiftValue >= 0
-          then shiftL word (fromIntegral shiftValue)
-          else shiftR word (fromIntegral (- shiftValue))
-      filledUpLastByte = lastByte `xor` leftByte wordAdjusted
-      shiftedRestOfWord = shiftL word (fromIntegral (unusedLeft + unusedRight))
-      resultUnused = mod (unusedLeft + unusedRight) 8
-   in if usedRight - (unusedLeft + unusedRight) < 8
-        then
-          (bs `snoc` filledUpLastByte)
-            :> Unaligned (leftByte shiftedRestOfWord) (8 - resultUnused)
-        else
-          (bs `snoc` filledUpLastByte `snoc` leftByte shiftedRestOfWord)
-            :> Unaligned (rightByte shiftedRestOfWord) (8 - resultUnused)
+    RightOpenByteString ->
+    LeftOpen Word16 ->
+    RightOpenByteString 
+pushWord (bs :> (RightOpen lastByte usedLeft)) (LeftOpen word usedRight) =
+    let unusedLeft = 8 - usedLeft
+        unusedRight = 16 - usedRight
+        shiftValue = unusedRight - usedLeft
+        wordAdjusted =
+            if shiftValue >= 0
+                then shiftL word (fromIntegral shiftValue)
+                else shiftR word (fromIntegral (- shiftValue))
+        filledUpLastByte = lastByte `xor` leftByte wordAdjusted
+        shiftedRestOfWord = shiftL word (fromIntegral (unusedLeft + unusedRight))
+        resultUnused = mod (unusedLeft + unusedRight) 8
+     in if usedRight - (unusedLeft + unusedRight) < 8
+            then
+                (bs `snoc` filledUpLastByte)
+                    :> RightOpen (leftByte shiftedRestOfWord) (8 - resultUnused)
+            else
+                (bs `snoc` filledUpLastByte `snoc` leftByte shiftedRestOfWord)
+                    :> RightOpen (rightByte shiftedRestOfWord) (8 - resultUnused)
 
--- | Take a Word16 from an unaligned right-packed Bytestring
-takeWord :: UnalignedBytestring 'LeftOpen -> Int -> Maybe Word16
-takeWord ((Unaligned byte used) :< bs) numberOfBits =
-    let numberOfBitsNotFromFirstByte = numberOfBits - used
-        word = fromIntegral byte :: Word16
-        adjustedFirstByte =
-          if numberOfBitsNotFromFirstByte < 0
-            then shiftR word (- numberOfBitsNotFromFirstByte)
-            else shiftL word numberOfBitsNotFromFirstByte
-     in 
-         fitIn numberOfBitsNotFromFirstByte bs (fromIntegral adjustedFirstByte)
-  where
-    additionalBytes :: Maybe ByteString
-    additionalBytes =
-      let numberOfNeededBytes = minBytes (numberOfBits - used)
-       in if BS.length bs >= numberOfNeededBytes
-            then Just (BS.take numberOfNeededBytes bs)
-            else Nothing
-    fitIn :: Int -> ByteString -> Word16 -> Maybe Word16
-    fitIn numberOfBits bs word
-      | numberOfBits == 0 = Just word
-      | numberOfBits > 8 =
-        if BS.length bs >= minBytes numberOfBits
-          then
-            fitIn
-               (numberOfBits - 8)
-               (BS.tail bs)
-            $  word `xor` shiftL (fromIntegral $ BS.head bs) (numberOfBits - 8)
-          else Nothing
-      | otherwise =
-        Just $ word `xor` shiftR (fromIntegral $ BS.head bs) (8 - numberOfBits)
+ -- | Take a Word16 from an unaligned right-packed Bytestring
+takeWord :: LeftOpenByteString -> Int -> Maybe Word16
+takeWord ((LeftOpen byte used) :< bs) numberOfBits =
+  let numberOfBitsNotFromFirstByte = numberOfBits - used
+      word = fromIntegral byte :: Word16
+      adjustedFirstByte =
+        if numberOfBitsNotFromFirstByte < 0
+           then shiftR word (- numberOfBitsNotFromFirstByte)
+           else shiftL word numberOfBitsNotFromFirstByte
+   in 
+    fitIn numberOfBitsNotFromFirstByte bs (fromIntegral adjustedFirstByte)
+      where
+         additionalBytes :: Maybe ByteString
+         additionalBytes =
+                 let numberOfNeededBytes = minBytes (numberOfBits - used)
+                   in if BS.length bs >= numberOfNeededBytes
+                        then Just (BS.take numberOfNeededBytes bs)
+                        else Nothing
+         fitIn :: Int -> ByteString -> Word16 -> Maybe Word16
+         fitIn numberOfBits bs word
+           | numberOfBits == 0 = Just word
+           | numberOfBits > 8 =
+               if BS.length bs >= minBytes numberOfBits
+                  then
+                   fitIn
+                      (numberOfBits - 8)
+                      (BS.tail bs)
+                   $  word `xor` shiftL (fromIntegral $ BS.head bs) (numberOfBits - 8)
+                  else Nothing
+           | otherwise =
+                 Just $ word `xor` shiftR (fromIntegral $ BS.head bs) (8 - numberOfBits)
 
 
 -- Helpers
@@ -192,6 +169,6 @@ takeWord ((Unaligned byte used) :< bs) numberOfBits =
 -- | Determine the number of Bytes needed to contain a number of bits.
 minBytes :: Int -> Int
 minBytes numberOfBits =
-  numberOfBits `div` 8 + fragmentCorrection
-  where
-    fragmentCorrection = if numberOfBits `mod` 8 == 0 then 0 else 1
+             numberOfBits `div` 8 + fragmentCorrection
+         where
+           fragmentCorrection = if numberOfBits `mod` 8 == 0 then 0 else 1
--- tests/UnalignedTests.hs
+++ tests/UnalignedTests.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
@@ -19,35 +17,35 @@ main = hspec $ do
         \x -> combineTwoBytes (leftByte x) (rightByte x) `shouldBe` (x :: Word16)
 
     it "Test smart constructors" $ do
-      makeUnaligned (65535 :: Word16) 8
-        `shouldBe` (Unaligned (255 :: Word16) 8 :: (Unaligned 'LeftOpen Word16))
-      makeUnaligned (65535 :: Word16) 11
-        `shouldBe` (Unaligned (65504 :: Word16) 11 :: (Unaligned 'RightOpen Word16))
+      makeLeftOpen (65535 :: Word16) 8
+        `shouldBe` LeftOpen 255 8
+      makeRightOpen (65535 :: Word16) 11
+        `shouldBe` RightOpen 65504 11
 
     it "Test the results of pushing an unaligned word onto a ByteString" $ do
       let (bs :> unfinished) =
             pushWord
-              ((empty :> Unaligned 128 1) :: UnalignedBytestring 'RightOpen)
-              (Unaligned (256 + 255) 9 :: Unaligned 'LeftOpen Word16)
+              (empty :> RightOpen 128 1)
+              (LeftOpen (256 + 255) 9)
        in do
             BS.last bs `shouldBe` 255
-            unfinished `shouldBe` Unaligned (128 + 64) 2
+            unfinished `shouldBe` RightOpen (128 + 64) 2
 
       let (bs :> unfinished) =
             pushWord
-              ((empty :> Unaligned 254 7) :: UnalignedBytestring 'RightOpen)
-              (Unaligned 65535 16 :: Unaligned 'LeftOpen Word16)
+              (empty :> RightOpen 254 7)
+              (LeftOpen 65535 16)
        in do
             BS.last bs `shouldBe` 255
-            unfinished `shouldBe` Unaligned 254 7
+            unfinished `shouldBe` RightOpen 254 7
 
     it "make mask for half of a byte, zeroes left" $ do
       makeMask 4 `shouldBe` 15
       makeMask 4 `shouldBe` 15
 
     it "test takeWord" $ do
-      let unalignedBs1 = (Unaligned 1 1 :< (127 `BS.cons` empty))
-          unalignedBs2 = (Unaligned 1 1 :< (255 `BS.cons` empty))
+      let unalignedBs1 = (LeftOpen 1 1 :< (127 `BS.cons` empty))
+          unalignedBs2 = (LeftOpen 1 1 :< (255 `BS.cons` empty))
        in do
             takeWord unalignedBs1 9 `shouldBe` Just (256 + 127)
             takeWord unalignedBs2 9 `shouldBe` Just (256 + 128 + 127)
@@ -57,19 +55,19 @@ main = hspec $ do
       minBytes 8 `shouldBe` 1
       minBytes 9 `shouldBe` 2
 
-    it "test makeUnaligned RightOpen ByteString" $ do
-      makeUnalignedByteString @(UnalignedBytestring 'RightOpen) BS.empty 8 `shouldBe` Nothing
-      let Just (bs :> (Unaligned integral n :: Unaligned 'RightOpen Word8)) =
-            makeUnalignedByteString @(UnalignedBytestring 'RightOpen)(2 `cons` 255 `cons` empty) 4
+    it "test makeRightOpenByteString" $ do
+      makeRightOpenByteString BS.empty 8 `shouldBe` Nothing
+      let Just (bs :> (RightOpen integral n :: RightOpen Word8)) =
+            makeRightOpenByteString (2 `cons` 255 `cons` empty) 4
        in do
             integral `shouldBe` 240
             n `shouldBe` 4
             bs `shouldBe` 2 `cons` empty
 
     it "test makeUnaligned LeftOpen ByteString" $ do
-      makeUnalignedByteString @(UnalignedBytestring 'LeftOpen) BS.empty 8 `shouldBe` Nothing
-      let Just ((Unaligned integral n :: Unaligned 'LeftOpen Word8) :< bs) =
-            makeUnalignedByteString @(UnalignedBytestring 'LeftOpen) (255 `cons` 2 `cons` empty) 4
+      makeLeftOpenByteString BS.empty 8 `shouldBe` Nothing
+      let Just ((LeftOpen integral n) :< bs) =
+            makeLeftOpenByteString  (255 `cons` 2 `cons` empty) 4
        in do
             integral `shouldBe` 15
             n `shouldBe` 4
